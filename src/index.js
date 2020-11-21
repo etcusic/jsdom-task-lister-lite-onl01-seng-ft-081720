ece4f5a4d950f9dc19fb8e80229d45871cbb23ee
Eli Cusic <tntitans31@gmail.com>
2020-11-21 09:09:47 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const list = document.getElementById('tasks')
+
+const taskButton = document.getElementById('task-button')
+
+function makeDatList(taskList) {
+    const node = document.createElement("LI")
+    const textnode = document.createTextNode(taskList)
+    node.appendChild(textnode)
+    document.getElementById("tasks").appendChild(node)
+}
+
+taskButton.addEventListener("click", function(event) {
+    const taskList = document.getElementById("new-task-description").value
+    console.log(taskList)
+    makeDatList(taskList)
+})
